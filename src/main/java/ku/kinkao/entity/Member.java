e9c875678112f6375e91c34f26d544d7fe63b4d9
Sittanat <apalakawongna@gmail.com>
2024-11-20 02:15:18 +0700

    Initial commit

---
+++ src/main/java/ku/kinkao/entity/Member.java
@@ -0,0 +1,36 @@
+package ku.kinkao.entity;
+
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import ku.kinkao.config.AttributeEncryptor;
+import lombok.Data;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Entity
+
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+
+    private String username;
+    private String password;
+    @Convert(converter = AttributeEncryptor.class)
+    private String firstName;
+
+    @Convert(converter = AttributeEncryptor.class)
+    private String lastName;
+
+    @Convert(converter = AttributeEncryptor.class)
+    private String email;
+    private String role;
+    private Instant createdAt;
+
+}
